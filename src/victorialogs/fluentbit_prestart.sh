92e454cfbf47d84a2a1feabef3e73363ac122b3e
Bereket Abraham <babraham@figma.com>
2024-09-05 22:15:20 -0700

    enable fluentbit to access journald logs
    Add some needed volume mounts.
    Also improve the prestart script to autodetect the right line number.

--- src/victorialogs/fluentbit_prestart.sh
+++ src/victorialogs/fluentbit_prestart.sh
@@ -6,10 +6,17 @@
 set -euo pipefail
 
 rm -f /etc/opt/fluentbit/config.conf
-head -n16 /etc/opt/fluentbit/config_template.conf > /etc/opt/fluentbit/config.conf
+
+line_num=$(sed -n '/PLACEHOLDER/=' /etc/opt/fluentbit/config_template.conf)
+if ! [[ $line_num =~ ^[0-9]+$ ]]; then
+  echo "error: could not locate end of input section" >&2
+  exit 1
+fi
+
+head -n $((line_num - 1)) /etc/opt/fluentbit/config_template.conf > /etc/opt/fluentbit/config.conf
 
 /usr/local/bin/list_services.sh | \
   xargs -I% echo "    Systemd_filter _SYSTEMD_UNIT=%.service" >> \
   /etc/opt/fluentbit/config.conf
 
-tail -n+17 /etc/opt/fluentbit/config_template.conf >> /etc/opt/fluentbit/config.conf
+tail -n +$((line_num + 1)) /etc/opt/fluentbit/config_template.conf >> /etc/opt/fluentbit/config.conf
