8e22899ca8c23aa595426fecd467ee25e5cfc471
Bereket Abraham <babraham@figma.com>
2025-07-06 14:03:33 -0700

    fix notification bugs

--- src/ntfy/chat.sh
+++ src/ntfy/chat.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # Sends a push notification to all users.
 # Usage:
-#   /root/homelab-rendered/src/ntfy/chat.sh TITLE SUBJECT [PRIORITY]
+#   /root/homelab-rendered/src/ntfy/chat.sh TITLE MESSAGE [PRIORITY]
 
 set -euo pipefail
 
 title="$1"
-subject="$2"
+msg="$2"
 priority="${3:-3}"
 password=$(/root/homelab-rendered/src/podman/get_secret.sh "ntfy_person_password")
-hash=$(echo "person:$password" | base64)
+hash=$(echo -n "person:$password" | base64)
 curl -X POST \
   -H "Authorization: Basic $hash" \
   -H "X-Title: $title" \
   -H "X-Priority: $priority" \
-  -d "$subject" https://push.{{ site.url }}/chat
+  -d "$msg" https://push.{{ site.url }}/chat
