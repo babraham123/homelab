b79704569a6d7a7c8dfe23fb443dda6b20c03529
Bereket Abraham <babraham42@gmail.com>
2026-03-19 13:13:03 -0700

    move the ssh port on the vpn server

--- tools/upload_src.sh
+++ tools/upload_src.sh
@@ -16,9 +16,14 @@ if ! ping -c3 -W3 "$host.$url" > /dev/null; then
   return
 fi
 
-scp -qr -o LogLevel=QUIET "$project_dir" "manualadmin@$host.$url:/home/manualadmin"
+port="22"
+if [[ "$host" == "vpn" ]]; then
+  port="2202"
+fi
+
+scp -qr -P "$port" -o LogLevel=QUIET "$project_dir" "manualadmin@$host.$url:/home/manualadmin"
 echo "$host root password:"
-ssh -t "manualadmin@$host.$url" '
+ssh -p "$port" -t "manualadmin@$host.$url" '
 sudo chown -R root:root homelab-rendered
 sudo rm -rf /root/homelab-rendered
 sudo mv homelab-rendered /root/homelab-rendered
